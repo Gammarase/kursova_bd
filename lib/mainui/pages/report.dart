2daff2d4842b6cf05e0166119bb3f38546de1c36
Romu4h <roman.shevchuk.job@gmail.com>
2022-12-05 13:32:07 +0200

    add search and demo add-button2

--- lib/mainui/pages/report.dart
+++ lib/mainui/pages/report.dart
@@ -8,7 +8,7 @@ class ReportPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('ReportPage'),
+        child: Text('ReportPage1'),
       ),
     );
   }
